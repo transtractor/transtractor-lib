5e69e64db62f8661766ab95dfc08678801eccbfd
New Name <gravytoast@pm.me>
2025-09-29 21:58:20 +1000

    Export StatementConfig and StatementData

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,9 +1,13 @@
 pub mod text_item;
 pub mod text_items;
 pub mod proto_transaction;
+pub mod statement_config;
+pub mod statement_data;
 pub mod transaction;
 
 pub use text_item::TextItem;
 pub use text_items::TextItems;
 pub use proto_transaction::ProtoTransaction;
+pub use statement_config::StatementConfig;
+pub use statement_data::StatementData;
 pub use transaction::Transaction;
