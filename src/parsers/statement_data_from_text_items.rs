aba8ea27cdc3f5e7d7112fbbb0b3890d68565dd1
New Name <gravytoast@pm.me>
2025-10-16 21:47:40 +1100

    Preliminary working parser

---
+++ src/parsers/statement_data_from_text_items.rs
@@ -0,0 +1,34 @@
+use crate::parsers::statement::OpeningBalanceParser;
+use crate::structs::StatementConfig;
+use crate::structs::StatementData;
+use crate::structs::TextItems;
+
+pub fn parse(config: &StatementConfig, text_items: &TextItems) -> StatementData {
+    let mut statement_data = StatementData::new();
+
+    // Initialize parsers 
+    let mut opening_balance_parser = OpeningBalanceParser::new(config);
+
+    // Other settings based on parsers
+    let max_lookahead = opening_balance_parser.get_max_lookahead();
+
+    // Iterate through text items, attempting to match account_terms
+    let len = text_items.len();
+    if len == 0 {
+        return statement_data;
+    }
+    let mut i: usize = 0;
+    while i < len {
+        let buffer_size = max_lookahead.min(len - i);
+        let buffer = text_items.get_text_item_buffer(i, buffer_size);
+        let consumed = opening_balance_parser.parse_items(&buffer, &mut statement_data);
+        if consumed > 0 {
+            i += consumed;
+            continue;
+        }
+
+        // No parser matched, move to next item
+        i += 1;
+    }
+    statement_data
+}
