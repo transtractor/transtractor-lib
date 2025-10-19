b726e7e4c0ff951511394750cf589f6b0eafc5be
New Name <gravytoast@pm.me>
2025-10-19 15:05:07 +1100

    Cache start date year when set

--- src/structs/statement_data.rs
+++ src/structs/statement_data.rs
@@ -1,9 +1,10 @@
 use crate ::structs::ProtoTransaction;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, TimeZone, Datelike};
 
 #[derive(Clone)]
 pub struct StatementData {
     pub start_date: Option<i64>,
+    pub start_date_year: Option<i32>,
     pub opening_balance: Option<f64>,
     pub closing_balance: Option<f64>,
     pub proto_transactions: Vec<ProtoTransaction>,
@@ -13,6 +14,7 @@ impl StatementData {
     pub fn new() -> Self {
         Self {
             start_date: None,
+            start_date_year: None,
             opening_balance: None,
             closing_balance: None,
             proto_transactions: Vec::new(),
@@ -22,10 +24,12 @@ impl StatementData {
     pub fn opening_balance(&self) -> Option<f64> { self.opening_balance }
     pub fn closing_balance(&self) -> Option<f64> { self.closing_balance }
     pub fn start_date(&self) -> Option<i64> { self.start_date }
+    pub fn start_date_year(&self) -> Option<i32> { self.start_date_year }
 
     // Setters for the fields
     pub fn set_start_date(&mut self, date: i64) {
         self.start_date = Some(date);
+        self.start_date_year = Utc.timestamp_millis_opt(date).single().map(|dt| dt.year());
     }
 
     pub fn set_opening_balance(&mut self, balance: f64) {
