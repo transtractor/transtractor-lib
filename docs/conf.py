0f2eb9c7ed2e2d9194fca7eab428308dcabc8ef2
Daniel Weber <gravytoast@pm.me>
2025-12-21 19:58:35 +1100

    Change version to 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../python'))
 project = 'Transtractor'
 copyright = '2025, Daniel Weber'
 author = 'Daniel Weber'
-release = '1.0.0'
+release = '0.9.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
